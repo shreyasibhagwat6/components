286ce7022fa4925b6b912b7e4305953612895c3c
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-02-12 08:06:25 -0500

    tailwind installed

--- src/Button.js
+++ src/Button.js
@@ -10,7 +10,11 @@ function Button ({
     outline,
     rounded
 }) {
-    return <button>{children}</button>;
+    return (
+        <button className='px-3 py-1.5 border border-blue-600 bg-blue-500 text-white'>
+            {children}
+        </button>
+    );
 }
 
 Button.propTypes = {
